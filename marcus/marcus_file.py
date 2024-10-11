46648783e6b7107daa1f4a4abff70956876936fe
Marcus Neiva <marcus.neiva.carvalho@gmail.com>
2024-10-11 15:08:24 -0300

    Message

--- marcus/marcus_file.py
+++ marcus/marcus_file.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+# sempre começar com git pull
+# já criei o .env e lá estão as minhas chaves. No terminal fiz a instalação: pip install python-dotenv
+# depois instalei open ai com install openai
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+openai_client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
