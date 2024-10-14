2f35baa093015e731cd07f2b0bb362043c56670e
Jonas Gripa <maxjonas2@hotmail.com>
2024-10-14 13:56:40 -0300

    Changes

--- jonas/jonas_file.py
+++ jonas/jonas_file.py
@@ -1,23 +1,25 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from openai import OpenAI
+import os 
 
 load_dotenv()
 
-openai_client = OpenAI()
 
-response = openai_client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content" : "What is the capital of France?"}
-    ]
+client = OpenAI(
+  api_key=os.getenv('NVIDIA_API_KEY'),
+  base_url = "https://integrate.api.nvidia.com/v1",
 )
 
-response_content = response.choices[0].message.content
+completion = client.chat.completions.create(
+  model="meta/llama-3.1-405b-instruct",
+  messages=[{"role":"user","content":"What are the capitals of France and Russia?"}],
+  temperature=0.2,
+  top_p=0.7,
+  max_tokens=1024,
+  stream=True
+)
 
-print(response_content)
-
-
-
-
-
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
