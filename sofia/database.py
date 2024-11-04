2915ce33cb349785e68b3b48e3f00b979fced79c
Jonas Gripa <maxjonas2@hotmail.com>
2024-11-04 14:02:59 -0300

    Changes

--- sofia/database.py
+++ sofia/database.py
@@ -1,26 +1,27 @@
 import sqlite3 as sql
+import pandas as pd
 
 # File names
-customers_file = 'customers.csv'
+customers_file = 'data/Mall_Customers.csv'
 
 database = 'SQLChatbot.db'
 
 # Implement function that reads table columns and adds the table schema to the system message 
 
-def load_to_sqlite(df, table_name, conn):
+def load_to_sqlite(df : pd.DataFrame, table_name : str, conn : sql.Connection):
     df.to_sql(table_name, conn, if_exists='replace', index=False, chunksize=500)
     print(f"{table_name} table created successfully.")
 
 def run_pipeline():
     # Connect to the database
-    conn = sqlite3.connect(database)
+    conn = sql.connect(database)
     
     try:
         # Load CSV files
         customers_df = pd.read_csv(customers_file)
         
         # Load data into SQLite tables
-        load_to_sqlite(customers_df, 'customers', conn)
+        load_to_sqlite(customers_df, 'mall_customers', conn)
         
         print("Data pipeline completed successfully.")
     except Exception as e:
